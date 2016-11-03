0e4953a22b67872ec3dad140e56dd4214bb611ba
Mike Allison <mike@mikeallisononline.com>
2016-11-03 15:06:53 -0500

    merge contacts modal

--- src/contacts/list/index.module.js
+++ src/contacts/list/index.module.js
@@ -1,7 +1,9 @@
 import component from './list.component';
 import item from './item/item.component';
+import mergeContacts from './mergeContacts/mergeContacts.controller';
 
 export default angular.module('mpdx.contacts.list', [
     component,
-    item
+    item,
+    mergeContacts
 ]).name;
