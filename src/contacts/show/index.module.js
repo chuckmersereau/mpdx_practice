0a000495a9a5f2b5fc24030d8836609cc0fed829
Mike Allison <mike@mikeallisononline.com>
2016-11-04 16:40:45 -0500

    history component

--- src/contacts/show/index.module.js
+++ src/contacts/show/index.module.js
@@ -3,6 +3,7 @@ import completeTask from './completeTask/completeTask.controller';
 import component from './show.component';
 import details from './details/index.module';
 import donations from './donations/index.module';
+import history from './history/history.component';
 import info from './info/info.component';
 import people from './people/people.component';
 import person from './person/person.component';
@@ -15,6 +16,7 @@ export default angular.module('mpdx.contacts.show', [
     component,
     details,
     donations,
+    history,
     info,
     people,
     person,
