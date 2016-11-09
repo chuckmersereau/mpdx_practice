81d16348ebc05afb60cc80ad9e298f5ecbe95826
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-09 14:19:04 +1300

    skip translation upload on travis

--- script.sh
+++ script.sh
@@ -4,7 +4,6 @@ echo '-- run tests --'
 npm test
 echo '-- run eslint --'
 eslint .
-
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
 
@@ -15,10 +14,14 @@ else
     export NODE_ENV=staging
 fi
 echo '-- run translation --'
-gulp extract
-bundle install
-bundle exec ruby onesky/upload.rb
-bundle exec ruby onesky/download.rb
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+    gulp extract
+    bundle install
+    bundle exec ruby onesky/upload.rb
+    bundle exec ruby onesky/download.rb
+else
+    echo 'Skipping translation because the current build is a pull request.'
+fi
 echo '-- run build --'
 npm run build
 echo '<!-- COMMIT:' $TRAVIS_COMMIT '-->' >> public/index.html
