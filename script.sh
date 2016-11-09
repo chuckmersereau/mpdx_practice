673ebc481f0437baca51eb917c371ba7c5d796f0
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-09 14:23:01 +1300

    update script to include upload only on non-pr related commits

--- script.sh
+++ script.sh
@@ -14,14 +14,14 @@ else
     export NODE_ENV=staging
 fi
 echo '-- run translation --'
+gulp extract
+bundle install
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
-    gulp extract
-    bundle install
     bundle exec ruby onesky/upload.rb
-    bundle exec ruby onesky/download.rb
 else
-    echo 'Skipping translation because the current build is a pull request.'
+    echo 'Skipping translation upload because the current build is a pull request.'
 fi
+bundle exec ruby onesky/download.rb
 echo '-- run build --'
 npm run build
 echo '<!-- COMMIT:' $TRAVIS_COMMIT '-->' >> public/index.html
