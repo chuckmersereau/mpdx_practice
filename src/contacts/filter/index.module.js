21368202a4efaa0a6df5847a91db819fc422aac6
Timothy McKee <railshead.mckee@gmail.com>
2016-11-10 13:31:22 -0500

    added filter component

--- src/contacts/filter/index.module.js
+++ src/contacts/filter/index.module.js
@@ -1,7 +1,10 @@
 import daterange from './daterange/daterange.component';
+import filter from './filter.component';
 import tags from './tags/tags.component';
+
 
 export default angular.module('mpdx.contacts.filter', [
     daterange,
+    filter,
     tags
 ]).name;
