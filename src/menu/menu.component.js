c37b7d491856ff19afbf294c2733848e9495a571
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-17 09:57:45 +1300

    add current user to beacon

--- src/menu/menu.component.js
+++ src/menu/menu.component.js
@@ -1,7 +1,12 @@
+/*global HS*/
 class menuController {
     constructor($state) {
         this.state = $state;
         this.isInState = (match) => $state.$current.name.indexOf(match) === 0;
+    }
+
+    showHelp() {
+        HS.beacon.open();
     }
 }
 
