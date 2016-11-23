9da6f4180274276ce0225cf3d6ab1535c5174361
Mike Allison <mike@mikeallisononline.com>
2016-11-23 10:17:26 -0600

    more edit functionality

--- src/donations/edit/edit.controller.js
+++ src/donations/edit/edit.controller.js
@@ -1,12 +1,19 @@
 class EditDonationController {
+    currencyService;
     currentAccountList;
 
     constructor(
-        contactsService, currentAccountList, donationsService,
+        blockUI,
+        contactsService, currencyService, currentAccountList, donationsService,
         donationId
     ) {
+        this.currencyService = currencyService;
         this.currentAccountList = currentAccountList;
         this.donationId = donationId;
+
+        this.blockUI = blockUI.instances.get('donationEdit');
+        this.blockUI.start();
+
         contactsService.getDonorAccounts();
         this.donorAccounts = contactsService.donorAccounts;
         if (donationsService.data === null) {
@@ -19,6 +26,7 @@ class EditDonationController {
     }
     loadingFinished(data) {
         this.donation = _.find(data.donations, { id: parseInt(this.donationId) });
+        this.blockUI.stop();
         console.log(this.donation);
     }
 }
