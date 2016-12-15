c05bbd34713513289d53cd55476162322f0f8fe8
Mike Allison <mike@mikeallisononline.com>
2016-12-15 13:41:43 -0600

    preferencesImports

--- src/preferences/import/import.component.js
+++ src/preferences/import/import.component.js
@@ -1,15 +1,15 @@
 class ImportPreferencesController {
     alerts;
-    importsService;
+    preferencesImports;
 
     constructor(
-        $filter, $state, $stateParams, importsService, alerts, help
+        $filter, $state, $stateParams, preferencesImports, alerts, help
     ) {
         this.$filter = $filter;
         this.$state = $state;
         this.$stateParams = $stateParams;
-        this.importsService = importsService;
         this.alerts = alerts;
+        this.preferencesImports = preferencesImports;
 
         this.saving = false;
         this.tabId = '';
@@ -40,7 +40,7 @@ class ImportPreferencesController {
         } else {
             this.tabId = service;
             if (service === 'google') {
-                this.importsService.load();
+                this.preferencesImports.load();
             }
             this.$state.go('preferences.imports.tab', { id: service }, { notify: false });
         }
@@ -49,18 +49,18 @@ class ImportPreferencesController {
         return this.tabId === service;
     }
     loadTags(query) {
-        return this.$filter('filter')(this.importsService.data.tags, { text: query });
+        return this.$filter('filter')(this.preferencesImports.data.tags, { text: query });
     }
 
     checkAllGoogleContactGroups() {
-        this.importsService.google_contact_import.groups = this.importsService.selected_account.contact_groups.map(item => item.id);
+        this.preferencesImports.google_contact_import.groups = this.preferencesImports.selected_account.contact_groups.map(item => item.id);
     }
     uncheckAllGoogleContactGroups() {
-        this.importsService.google_contact_import.groups = [];
+        this.preferencesImports.google_contact_import.groups = [];
     }
     saveGoogleImport() {
         this.saving = true;
-        this.importsService.saveGoogleImport().then(() => {
+        this.preferencesImports.saveGoogleImport().then(() => {
             this.alerts.addAlert('MPDx is importing contacts from your Google Account', 'success');
             this.setTab('');
             this.saving = false;
