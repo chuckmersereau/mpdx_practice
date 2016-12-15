c770bc9622ebf4d9a0d2009b9662a383fc87602a
Mike Allison <mike@mikeallisononline.com>
2016-12-15 15:47:31 -0600

    merge

--- src/contacts/show/show.component.js
+++ src/contacts/show/show.component.js
@@ -8,7 +8,7 @@ class ContactController {
     tasksService;
 
     constructor(
-        $scope, $state, $stateParams, $location, $anchorScroll, help
+        $scope, $state, $stateParams, $location, $anchorScroll, help,
         modal, contacts, tasksService, contactReferrals, preferencesContacts, contactFilter, serverConstants
     ) {
         this.$anchorScroll = $anchorScroll;
