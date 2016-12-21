1a88f4ceb02f3e5f50d8918e6f652c8c856db0e1
Mike Allison <mike@mikeallisononline.com>
2016-12-21 13:37:21 -0600

    update entity models

--- src/common/api/api.service.js
+++ src/common/api/api.service.js
@@ -21,7 +21,7 @@ class Api {
         this.apiUrl = config.apiUrl;
         this.apiCache = $cacheFactory('api');
         this.account_list_id = null;
-        this.entityAttributes = new entityAttributes().attributes;
+        this.entityAttributes = new EntityAttributes().attributes;
 
         // This function supports both callbacks (successFn, errorFn) and returns a promise
         // It would be preferred to use promises in the future
@@ -72,11 +72,12 @@ class Api {
             transformRequest: (data) => {
                 let key = null;
                 let params = {keyForAttribute: 'underscore_case'};
-                if (method === 'put') {
+                if (method === 'put' || method === 'post') {
                     let arr = url.split('/');
                     key = arr[arr.length - 2];
-                    params.attributes = _.get(this.entityAttributes, key, null);
-                    if (!params.attributes) {
+                    if (_.has(this.entityAttributes, key)) {
+                        _.extend(params, this.entityAttributes[key]);
+                    } else {
                         this.$log.error(`undefined attributes for model: ${key} in api.service`);
                     }
                 }
@@ -150,14 +151,71 @@ class Api {
     }
 }
 
-class entityAttributes {
+//This class provides all of the meta information needed to serialize jsonapi data
+class EntityAttributes {
     constructor() {
         this.attributes = {
-            'people': ["first_name", "legal_first_name", "last_name", "birthday_month", "birthday_year", "birthday_day", "anniversary_month", "anniversary_year", "anniversary_day", "title",
-                "suffix", "gender", "marital_status", "preferences", "sign_in_count", "current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "created_at", "updated_at",
-                "master_person_id", "middle_name", "access_token", "profession", "deceased", "subscribed_to_updates", "optout_enewsletter", "occupation", "employer", "not_duplicated_with"],
-            'tasks': ["account_list_id", "starred", "location", "subject", "start_at", "end_at", "type", "created_at", "updated_at", "completed", "activity_comments_count", "activity_type", "result",
-                "completed_at", "notification_id", "remote_id", "source", "next_action", "no_date", "notification_type", "notification_time_before", "notification_time_unit", "notification_scheduled"]
+            account_lists: {
+                attributes: ["name", "creator_id", "created_at", "updated_at", "settings"]
+            },
+            appeals: {
+                attributes: ["created_at", "updated_at", "amount", "currencies", "description", "donations", "end_date", "name", "total_currency", "contacts"]
+            },
+            contacts: {
+                attributes: ["name", "account_list_id", "created_at", "updated_at", "pledge_amount", "status", "total_donations", "last_donation_date", "first_donation_date", "notes", "notes_saved_at",
+                    "full_name", "greeting", "website", "pledge_frequency", "pledge_start_date", "next_ask", "likely_to_give", "church_name", "send_newsletter", "direct_deposit", "magazine", "last_activity",
+                    "last_appointment", "last_letter", "last_phone_call", "last_pre_call", "last_thank", "pledge_received", "tnt_id", "not_duplicated_with", "uncompleted_tasks_count", "prayer_letters_id",
+                    "timezone", "envelope_greeting", "no_appeals", "prayer_letters_params", "pls_id", "pledge_currency", "locale", "late_at"]
+            },
+            donations: {
+                attributes: ["remote_id", "donor_account_id", "designation_account_id", "motivation", "payment_method", "tendered_currency", "tendered_amount", "currency", "amount", "memo",
+                    "donation_date", "created_at", "updated_at", "payment_type", "channel", "appeal_id", "appeal_amount"]
+            },
+            email_addresses: {
+                attributes: ["person_id", "email", "primary", "created_at", "updated_at", "remote_id", "location", "historic", "email"]
+            },
+            family_relationships: {
+                attributes: ["person_id", "related_person_id", "relationship", "created_at", "updated_at"]
+            },
+            notifications: {
+                attributes: ["contact_id", "notification_type_id", "event_date", "cleared", "created_at", "updated_at", "donation_id"]
+            },
+            people: {
+                attributes: ["first_name", "legal_first_name", "last_name", "birthday_month", "birthday_year", "birthday_day", "anniversary_month", "anniversary_year", "anniversary_day", "title",
+                    "suffix", "gender", "marital_status", "preferences", "sign_in_count", "current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "created_at", "updated_at",
+                    "master_person_id", "middle_name", "access_token", "profession", "deceased", "subscribed_to_updates", "optout_enewsletter", "occupation", "employer", "not_duplicated_with"]
+            },
+            person_facebook_accounts: {
+                attributes: ["person_id", "remote_id", "token", "token_expires_at", "created_at", "updated_at", "valid_token", "first_name", "last_name", "authenticated", "downloading", "last_download", "username"]
+            },
+            person_google_accounts: {
+                attributes: ["remote_id", "person_id", "token", "refresh_token", "expires_at", "valid_token", "created_at", "updated_at", "email", "authenticated", "primary", "downloading", "last_download", "last_email_sync", "notified_failure"]
+            },
+            person_key_accounts: {
+                attributes: ["person_id", "relay_remote_id", "first_name", "last_name", "email", "designation", "employee_id", "username", "authenticated", "created_at", "updated_at", "primary", "downloading", "last_download", "remote_id"]
+            },
+            person_linkedin_accounts: {
+                attributes: ["person_id", "remote_id", "token", "secret", "token_expires_at", "created_at", "updated_at", "valid_token", "first_name", "last_name", "authenticated", "downloading", "last_download", "public_url"]
+            },
+            person_organization_accounts: {
+                attributes: ["person_id", "organization_id", "username", "password", "created_at", "updated_at", "remote_id", "authenticated", "valid_credentials", "downloading", "last_download", "token", "locked_at", "disable_downloads"]
+            },
+            person_twitter_accounts: {
+                attributes: ["person_id", "remote_id", "screen_name", "token", "secret", "created_at", "updated_at", "valid_token", "authenticated", "primary", "downloading", "last_download"]
+            },
+            person_websites: {
+                attributes: ["person_id", "url", "primary", "created_at", "updated_at"]
+            },
+            phone_numbers: {
+                attributes: ["person_id", "number", "country_code", "location", "primary", "created_at", "updated_at", "remote_id", "historic"]
+            },
+            tasks: {
+                attributes: ["account_list_id", "starred", "location", "subject", "start_at", "end_at", "type", "created_at", "updated_at", "completed", "activity_comments_count", "activity_type", "result",
+                    "completed_at", "notification_id", "remote_id", "source", "next_action", "no_date", "notification_type", "notification_time_before", "notification_time_unit", "notification_scheduled"]
+            },
+            users: {
+                attributes: ["first_name", "preferences", "setup", "email", "access_token", "time_zone", "locale"]
+            }
         };
     }
 }
