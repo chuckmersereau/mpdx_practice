51adffc68e2323c38ca78cced57cc8841b368002
Mike Allison <mike@mikeallisononline.com>
2016-12-23 12:44:28 -0600

    comment out updated_in_db_at code in anticipation of it being included in request

--- src/common/api/api.service.js
+++ src/common/api/api.service.js
@@ -22,6 +22,8 @@ class Api {
         this.apiCache = $cacheFactory('api');
         this.account_list_id = null;
         this.entityAttributes = new EntityAttributes().attributes;
+        // add updated_in_db_at to all entities (for server sync)
+        // _.each(this.entityAttributes, entity => entity.attributes.push("updated_in_db_at"));
 
         // This function supports both callbacks (successFn, errorFn) and returns a promise
         // It would be preferred to use promises in the future
@@ -96,6 +98,7 @@ class Api {
                 if (!_.isString(data)) {
                     return new japi.Deserializer({keyForAttribute: 'underscore_case'}).deserialize(data).then((data) => {
                         data.meta = meta;
+                        // data = this.addServerUpdateNested(data); //add updated_in_db_at timestamps
                         return data;
                     });
                 } else {
@@ -157,6 +160,17 @@ class Api {
     encodeURLarray(array) {
         return _.map(array, encodeURIComponent);
     }
+    addServerUpdateNested(obj) {
+        if (_.has(obj, 'updated_at')) {
+            obj.updated_in_db_at = obj.updated_at;
+        }
+        _.each(_.keys(obj), (key) => {
+            if (_.isArray(obj[key])) {
+                this.addServerUpdateNested(obj[key]);
+            }
+        });
+        return obj;
+    }
 }
 
 //This class provides all of the meta information needed to serialize jsonapi data
@@ -164,43 +178,43 @@ class EntityAttributes {
     constructor() {
         this.attributes = {
             account_list_invites: {
-                attributes: ["created_at", "updated_at", "accepted_at", "accepted_by_user_id", "account_list_id", "cancelled_by_user_id", "code", "invited_by_user_id", "recipient_email", "updated_at"]
+                attributes: ["created_at", "updated_at", "accepted_at", "accepted_by_user_id", "account_list_id", "cancelled_by_user_id", "code", "invited_by_user_id", "recipient_email", "updated_at", "updated_in_db_at"]
             },
             account_lists: {
-                attributes: ["name", "creator_id", "created_at", "updated_at", "settings"]
+                attributes: ["name", "creator_id", "created_at", "updated_at", "settings", "updated_in_db_at"]
             },
             addresses: {
-                attributes: ["street", "city", "country", "end_date", "geo", "historic", "location", "postal_code", "primary_mailing_address", "start_date", "state"]
+                attributes: ["street", "city", "country", "end_date", "geo", "historic", "location", "postal_code", "primary_mailing_address", "start_date", "state", "updated_in_db_at"]
             },
             appeals: {
-                attributes: ["created_at", "updated_at", "amount", "currencies", "description", "donations", "end_date", "name", "total_currency", "contacts"]
+                attributes: ["created_at", "updated_at", "amount", "currencies", "description", "donations", "end_date", "name", "total_currency", "contacts", "updated_in_db_at"]
             },
             contacts: {
                 attributes: ["name", "account_list_id", "created_at", "updated_at", "pledge_amount", "status", "total_donations", "last_donation_date", "first_donation_date", "notes", "notes_saved_at",
                     "full_name", "greeting", "website", "pledge_frequency", "pledge_start_date", "next_ask", "likely_to_give", "church_name", "send_newsletter", "direct_deposit", "magazine", "last_activity",
                     "last_appointment", "last_letter", "last_phone_call", "last_pre_call", "last_thank", "pledge_received", "tnt_id", "not_duplicated_with", "uncompleted_tasks_count", "prayer_letters_id",
-                    "timezone", "envelope_greeting", "no_appeals", "prayer_letters_params", "pls_id", "pledge_currency", "locale", "late_at", "people", "addresses"],
+                    "timezone", "envelope_greeting", "no_appeals", "prayer_letters_params", "pls_id", "pledge_currency", "locale", "late_at", "people", "addresses", "updated_in_db_at"],
                 addresses: () => _.extend({ ref: 'id' }, this.attributes.addresses),
                 people: () => _.extend({ ref: 'id' }, this.attributes.people)
             },
             donations: {
                 attributes: ["remote_id", "donor_account_id", "designation_account_id", "motivation", "payment_method", "tendered_currency", "tendered_amount", "currency", "amount", "memo",
-                    "donation_date", "created_at", "updated_at", "payment_type", "channel", "appeal_id", "appeal_amount"]
+                    "donation_date", "created_at", "updated_at", "payment_type", "channel", "appeal_id", "appeal_amount", "updated_in_db_at"]
             },
             email_addresses: {
-                attributes: ["person_id", "email", "primary", "created_at", "updated_at", "remote_id", "location", "historic", "email"]
+                attributes: ["person_id", "email", "primary", "created_at", "updated_at", "remote_id", "location", "historic", "email", "updated_in_db_at"]
             },
             family_relationships: {
-                attributes: ["person_id", "related_person_id", "relationship", "created_at", "updated_at"]
+                attributes: ["person_id", "related_person_id", "relationship", "created_at", "updated_at", "updated_in_db_at"]
             },
             notifications: {
-                attributes: ["contact_id", "notification_type_id", "event_date", "cleared", "created_at", "updated_at", "donation_id"]
+                attributes: ["contact_id", "notification_type_id", "event_date", "cleared", "created_at", "updated_at", "donation_id", "updated_in_db_at"]
             },
             people: {
                 attributes: ["first_name", "legal_first_name", "last_name", "birthday_month", "birthday_year", "birthday_day", "anniversary_month", "anniversary_year", "anniversary_day", "title",
                     "suffix", "gender", "marital_status", "preferences", "sign_in_count", "current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "created_at", "updated_at",
                     "master_person_id", "middle_name", "access_token", "profession", "deceased", "subscribed_to_updates", "optout_enewsletter", "occupation", "employer", "not_duplicated_with",
-                    "phone_numbers", "email_addresses", "facebook_accounts", "family_relationships", "linkedin_accounts", "websites"],
+                    "phone_numbers", "email_addresses", "facebook_accounts", "family_relationships", "linkedin_accounts", "websites", "updated_in_db_at"],
                 email_addresses: () => _.extend({ ref: 'id' }, this.attributes.email_addresses),
                 facebook_accounts: () => _.extend({ ref: 'id' }, this.attributes.person_facebook_accounts),
                 family_relationships: () => _.extend({ ref: 'id' }, this.attributes.family_relationships),
@@ -209,35 +223,35 @@ class EntityAttributes {
                 websites: () => _.extend({ ref: 'id' }, this.attributes.person_websites)
             },
             person_facebook_accounts: {
-                attributes: ["person_id", "remote_id", "token", "token_expires_at", "created_at", "updated_at", "valid_token", "first_name", "last_name", "authenticated", "downloading", "last_download", "username"]
+                attributes: ["person_id", "remote_id", "token", "token_expires_at", "created_at", "updated_at", "valid_token", "first_name", "last_name", "authenticated", "downloading", "last_download", "username", "updated_in_db_at"]
             },
             person_google_accounts: {
-                attributes: ["remote_id", "person_id", "token", "refresh_token", "expires_at", "valid_token", "created_at", "updated_at", "email", "authenticated", "primary", "downloading", "last_download", "last_email_sync", "notified_failure"]
+                attributes: ["remote_id", "person_id", "token", "refresh_token", "expires_at", "valid_token", "created_at", "updated_at", "email", "authenticated", "primary", "downloading", "last_download", "last_email_sync", "notified_failure", "updated_in_db_at"]
             },
             person_key_accounts: {
-                attributes: ["person_id", "relay_remote_id", "first_name", "last_name", "email", "designation", "employee_id", "username", "authenticated", "created_at", "updated_at", "primary", "downloading", "last_download", "remote_id"]
+                attributes: ["person_id", "relay_remote_id", "first_name", "last_name", "email", "designation", "employee_id", "username", "authenticated", "created_at", "updated_at", "primary", "downloading", "last_download", "remote_id", "updated_in_db_at"]
             },
             person_linkedin_accounts: {
-                attributes: ["person_id", "remote_id", "token", "secret", "token_expires_at", "created_at", "updated_at", "valid_token", "first_name", "last_name", "authenticated", "downloading", "last_download", "public_url"]
+                attributes: ["person_id", "remote_id", "token", "secret", "token_expires_at", "created_at", "updated_at", "valid_token", "first_name", "last_name", "authenticated", "downloading", "last_download", "public_url", "updated_in_db_at"]
             },
             person_organization_accounts: {
-                attributes: ["person_id", "organization_id", "username", "password", "created_at", "updated_at", "remote_id", "authenticated", "valid_credentials", "downloading", "last_download", "token", "locked_at", "disable_downloads"]
+                attributes: ["person_id", "organization_id", "username", "password", "created_at", "updated_at", "remote_id", "authenticated", "valid_credentials", "downloading", "last_download", "token", "locked_at", "disable_downloads", "updated_in_db_at"]
             },
             person_twitter_accounts: {
-                attributes: ["person_id", "remote_id", "screen_name", "token", "secret", "created_at", "updated_at", "valid_token", "authenticated", "primary", "downloading", "last_download"]
+                attributes: ["person_id", "remote_id", "screen_name", "token", "secret", "created_at", "updated_at", "valid_token", "authenticated", "primary", "downloading", "last_download", "updated_in_db_at"]
             },
             person_websites: {
-                attributes: ["person_id", "url", "primary", "created_at", "updated_at"]
+                attributes: ["person_id", "url", "primary", "created_at", "updated_at", "updated_in_db_at"]
             },
             phone_numbers: {
-                attributes: ["person_id", "number", "country_code", "location", "primary", "created_at", "updated_at", "remote_id", "historic"]
+                attributes: ["person_id", "number", "country_code", "location", "primary", "created_at", "updated_at", "remote_id", "historic", "updated_in_db_at"]
             },
             tasks: {
                 attributes: ["account_list_id", "starred", "location", "subject", "start_at", "end_at", "type", "created_at", "updated_at", "completed", "activity_comments_count", "activity_type", "result",
-                    "completed_at", "notification_id", "remote_id", "source", "next_action", "no_date", "notification_type", "notification_time_before", "notification_time_unit", "notification_scheduled"]
+                    "completed_at", "notification_id", "remote_id", "source", "next_action", "no_date", "notification_type", "notification_time_before", "notification_time_unit", "notification_scheduled", "updated_in_db_at"]
             },
             user: {
-                attributes: ["first_name", "preferences", "setup", "email", "access_token", "time_zone", "locale"]
+                attributes: ["first_name", "preferences", "setup", "email", "access_token", "time_zone", "locale", "updated_at", "updated_in_db_at"]
             }
         };
     }
