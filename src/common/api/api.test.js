3e48f99bd39d372bbdadf91d1e1843fa94ee9945
Mike Allison <mike@mikeallisononline.com>
2017-01-04 12:40:38 -0600

    fix api unit test. make api call errors not transform

--- src/common/api/api.test.js
+++ src/common/api/api.test.js
@@ -1,5 +1,24 @@
 import api from './api.service';
 
+const jsonApiResponse = {
+    data: {
+        success: true
+    }
+};
+
+const transformedResponse = {
+    success: true
+};
+
+const jsonApiErrorResponse = {
+    errors: [{
+        status: "bad_request",
+        source: {pointer: "/data/attributes/updated_in_db_at"},
+        title: "has to be sent in the list of attributes in order to update resource",
+        detail: "Updated in db at has to be sent in the list of attributes in order to update resource"
+    }]
+};
+
 describe('common.api.service', () => {
     let service, $httpBackend;
     beforeEach(() => {
@@ -17,32 +36,33 @@ describe('common.api.service', () => {
     describe('call', () => {
         describe('promise', () => {
             it('should send a simple get request', () => {
-                $httpBackend.expectGET('/api/v1/contacts').respond(200, 'Success');
+                $httpBackend.expectGET('/api/v1/contacts').respond(200, jsonApiResponse);
 
                 service.get('contacts').then((data) => {
-                    expect(data).toEqual('Success');
+                    expect(data).toEqual(transformedResponse);
                 }).catch(() => {
                     fail('should have returned Success');
                 });
                 $httpBackend.flush();
             });
             it('should handle an error in a get request', () => {
-                $httpBackend.expectGET('/api/v1/contacts').respond(500, 'Error');
+                $httpBackend.expectGET('/api/v1/contacts').respond(500, jsonApiErrorResponse);
 
                 service.get('contacts').then(() => {
                     fail('should have returned an error');
                 }).catch((response) => {
-                    expect(response.data).toEqual('Error');
+                    expect(response.status).toEqual(500);
+                    // expect(response.data.errors).toExist();
                 });
                 $httpBackend.flush();
             });
 
             describe('cache', () => {
                 it('should make a XHR request since it is not cached', () => {
-                    $httpBackend.expectGET('/api/v1/contacts').respond(200, 'Success');
+                    $httpBackend.expectGET('/api/v1/contacts').respond(200, jsonApiResponse);
 
                     service.get({ url: 'contacts', cache: false }).then((data) => {
-                        expect(data).toEqual('Success');
+                        expect(data).toEqual(transformedResponse);
                     }).catch(() => {
                         fail('should have returned Success');
                     });
@@ -50,11 +70,11 @@ describe('common.api.service', () => {
                 });
 
                 it('should make not make an XHR request the second time', () => {
-                    $httpBackend.expectGET('/api/v1/contacts').respond(200, 'Success');
+                    $httpBackend.expectGET('/api/v1/contacts').respond(200, jsonApiResponse);
 
                     function runApiCall() {
                         service.call({ method: 'get', url: 'contacts', cache: true }).then((data) => {
-                            expect(data).toEqual('Success');
+                            expect(data).toEqual(transformedResponse);
                         }).catch(() => {
                             fail('should have returned Success');
                         });
@@ -70,10 +90,10 @@ describe('common.api.service', () => {
 
     describe('get', () => {
         it('should send a simple get request', () => {
-            $httpBackend.expectGET('/api/v1/contacts').respond(200, 'Success');
+            $httpBackend.expectGET('/api/v1/contacts').respond(200, jsonApiResponse);
 
             service.get('contacts').then((data) => {
-                expect(data).toEqual('Success');
+                expect(data).toEqual(transformedResponse);
             }).catch(() => {
                 fail('should have returned Success');
             });
@@ -83,10 +103,10 @@ describe('common.api.service', () => {
 
     describe('post', () => {
         it('should send a simple post request', () => {
-            $httpBackend.expectPOST('/api/v1/contacts').respond(200, 'Success');
+            $httpBackend.expectPOST('/api/v1/contacts').respond(200, jsonApiResponse);
 
             service.post('contacts').then((data) => {
-                expect(data).toEqual('Success');
+                expect(data).toEqual(transformedResponse);
             }).catch(() => {
                 fail('should have returned Success');
             });
