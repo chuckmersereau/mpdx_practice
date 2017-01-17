036e08d5241fe7b39d48ea73d54352b2e3d40c26
Mike Allison <dj.mikeallison@gmail.com>
2017-01-17 09:49:14 -0600

    finished progress  analytics

--- src/home/progress/progress.component.js
+++ src/home/progress/progress.component.js
@@ -13,67 +13,27 @@ class progressController {
         this.blockUI = blockUI.instances.get('dashboardProgress');
         this.users = users;
 
-        this.startDate = new Date();
-        this.startDate.setHours(0, 0, 0, 0);
-        this.startDate.setDate(this.startDate.getDate() - this.startDate.getDay() + 1);
-        this.endDate = new Date(this.startDate);
-        this.endDate.setDate(this.startDate.getDate() + 7);
+        this.endDate = moment().subtract(1, 'day');
+        this.startDate = moment(this.endDate).subtract(1, 'week');
         this.errorOccurred = false;
     }
     blankData() {
         this.blockUI.start();
-        this.data = {
-            contacts: {
-                active: '-', referrals_on_hand: '-', referrals: '-'
-            },
-            phone: {
-                completed: '-',
-                attempted: '-',
-                received: '-',
-                appointments: '-',
-                talktoinperson: '-'
-            },
-            email: {
-                sent: '-',
-                received: '-'
-            },
-            facebook: {
-                sent: '-',
-                received: '-'
-            },
-            text_message: {
-                sent: '-',
-                received: '-'
-            },
-            electronic: {
-                sent: '-',
-                received: '-',
-                appointments: '-'
-            },
-            appointments: {
-                completed: '-'
-            },
-            correspondence: {
-                precall: '-',
-                support_letters: '-',
-                thank_yous: '-',
-                reminders: '-'
-            }
-        };
+        this.accounts.analytics = null;
     }
     nextWeek() {
-        this.startDate.setDate(this.startDate.getDate() + 7);
-        this.endDate.setDate(this.endDate.getDate() + 7);
+        this.startDate.add(1, 'week');
+        this.endDate.add(1, 'week');
         this.refreshData();
     }
     previousWeek() {
-        this.startDate.setDate(this.startDate.getDate() - 7);
-        this.endDate.setDate(this.endDate.getDate() - 7);
+        this.startDate.subtract(1, 'week');
+        this.endDate.subtract(1, 'week');
         this.refreshData();
     }
     refreshData() {
         this.blankData();
-        this.accounts.getAnalytics().then(() => {
+        this.accounts.getAnalytics({startDate: this.startDate, endDate: this.endDate}).then(() => {
             this.blockUI.stop();
         });
     }
