6023ca9e836ddba39ba08c7f5e402e7992236ee3
Mike Allison <mike@mikeallisononline.com>
2017-01-30 12:00:37 -0600

    remove old code

--- src/common/serverConstants/serverConstants.service.js
+++ src/common/serverConstants/serverConstants.service.js
@@ -11,26 +11,7 @@ class ServerConstantsService {
 
         this.data = null;
 
-        // this.data = {
-        //     contacts: {},
-        //     no_appeals_options: {},
-        //     mail_chimp_locale_options: {}
-        // };
-        // this.loading = true;
-
-        // this.isFetching = {
-        //     all: false,
-        //     actions: false,
-        //     nextActions: false,
-        //     results: false,
-        //     bulkUpdateOptions: false,
-        //     constants: {}
-        // };
-        //
-        // this.fetchPromises = {};
-
         // // FIXME temporary hack. remove when the endpoint '/constants' is working.
-        // this.data['actions'] = ['Call', 'Appointment', 'Email', 'Text Message', 'Facebook Message', 'Letter', 'Newsletter', 'Pre Call Letter', 'Reminder Letter', 'Support Letter', 'Thank', 'To Do', 'Talk to In Person', 'Prayer Request'];
         // let options = {};
         // options['Call'] = ['Call Again', 'Email', 'Text', 'Message', 'Talk to In Person', 'Cultivate Relationship', 'Appointment Scheduled', 'Partner - Financial', 'Partner - Special', 'Partner - Pray', 'Ask in Future', 'Not Interested', 'None', 'Prayer Request', 'Thank'];
         // options['Appointment'] = ['Call for Decision', 'Call', 'Email', 'Text', 'Message', 'Talk to In Person', 'Cultivate Relationship', 'Partner - Financial', 'Partner - Special', 'Partner - Pray', 'Ask in Future', 'Not Interested', 'Reschedule', 'None', 'Prayer Request', 'Thank'];
@@ -66,83 +47,6 @@ class ServerConstantsService {
             return data;
         });
     }
-    //tasks/next_actions   tasks/actions   tasks/results
-    // fetchConstant(constantName, url) {
-    //     if (!_.get(this.isFetching.constants, constantName, false)) {
-    //         this.isFetching.constants[constantName] = true;
-    //         let promise = this.api.get(url).then((data) => {
-    //             this.data[constantName] = data;
-    //             this.isFetching.constants[constantName] = false;
-    //         });
-    //         this.fetchPromises[constantName] = promise;
-    //         return promise;
-    //     }
-    //     return this.fetchPromises[constantName];
-    // }
-    // fetchConstants(constantsNames) {
-    //     if (this.isFetching.all === true) {
-    //         return this.fetchPromises.all;
-    //     }
-    //
-    //     let fetchConstantsNames = [];
-    //     let excludeConstantsNames = [];
-    //
-    //     if (constantsNames.length > 0) {
-    //         _.each(constantsNames, (constantName) => {
-    //             if (this._shouldFetch(constantName)) {
-    //                 fetchConstantsNames.push(constantName);
-    //                 this.isFetching.constants[constantName] = true;
-    //             }
-    //         });
-    //
-    //         if (fetchConstantsNames.length === 0) {
-    //             return this.$q.all(
-    //                 _.map(constantsNames, key => this.fetchPromises[key])
-    //             );
-    //         }
-    //     } else {
-    //         this.isFetching.all = true;
-    //         _.mapKeys(this.isFetching.constants, (val, key) => {
-    //             if (this.isFetching.constants[key] === true) {
-    //                 excludeConstantsNames.push(key);
-    //             }
-    //         });
-    //     }
-    //
-    //     let promise = this.api.get('constants',
-    //         {include: fetchConstantsNames.join(','), exclude: excludeConstantsNames.join(',')}
-    //     ).then((data) => {
-    //         _.mapKeys(data, (val, key) => {
-    //             this.data[key] = val;
-    //             this.isFetching.constants[key] = false;
-    //
-    //             if (fetchConstantsNames.length === 0) {
-    //                 this.isFetching.all = false;
-    //             }
-    //         });
-    //     });
-    //     const keys = _.difference(fetchConstantsNames, excludeConstantsNames);
-    //     _.each(keys, (key) => {
-    //         this.fetchPromises[key] = promise;
-    //     });
-    //     return promise;
-    // }
-    // _shouldFetch(constantName) {
-    //     return !(this.data[constantName] || this.isFetching.constants[constantName]);
-    // }
-    // list() {
-    //     return this.api.get('constants').then((data) => {
-    //         this.$log.debug('constants', data);
-            // _.mapKeys(data, (val, key) => {
-            //     this.data[key] = val;
-            //     this.isFetching.constants[key] = false;
-            //
-            //     if (fetchConstantsNames.length === 0) {
-            //         this.isFetching.all = false;
-            //     }
-            // });
-        // });
-    // }
 }
 
 export default angular.module('mpdx.common.serverConstants', [])
