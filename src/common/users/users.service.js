5d3dc7b9f6e648c06fba04fd6bfa86bd36ba2fb5
Mike Allison <mike@mikeallisononline.com>
2017-02-14 12:02:24 -0600

    fix initial setup_position value

--- src/common/users/users.service.js
+++ src/common/users/users.service.js
@@ -65,9 +65,9 @@ class Users {
             this.$log.debug('user/options', this.current.options);
             if (forRouting) {
                 if (!_.has(this.current.options, 'setup_position')) { //force first time setup
-                    return this.createOption('setup_position', 'start').then(() => {
-                        data.setup_position = {};
-                        this.current.options = this.mapOptions(data);
+                    return this.createOption('setup_position', 'start').then((pos) => {
+                        this.current.options.setup_position = pos;
+                        //  = this.mapOptions(data);
                         return this.$q.reject({redirect: 'setup.start'});
                     });
                 } else if (this.current.options.setup_position.value !== '') {
