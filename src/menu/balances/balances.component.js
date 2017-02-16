057b1635b45c4bd45eed1f161631a7495470b6e4
Mike Allison <mike@mikeallisononline.com>
2017-02-16 12:27:38 -0600

    add account balances component data

--- src/menu/balances/balances.component.js
+++ src/menu/balances/balances.component.js
@@ -1,30 +1,48 @@
 class BalancesController {
-    constructor(gettextCatalog) {
+    accounts;
+    api;
+    constructor(
+        $log, $rootScope, gettextCatalog,
+        accounts, api
+    ) {
+        this.$log = $log;
+        this.$rootScope = $rootScope;
+        this.accounts = accounts;
+        this.api = api;
         this.gettextCatalog = gettextCatalog;
+
+        this.balances = null;
+        this.goals = null;
     }
-    $onChanges(obj) {
-        if (obj.monthlyGoal) {
+    $onInit() {
+        this.init();
+        this.$rootScope.$on('accountListUpdated', () => {
+            this.init();
+        });
+    }
+    init() {
+        this.api.get('reports/goal_progress', {filter: {account_list_id: this.api.account_list_id}}).then((data) => {
+            this.$log.debug('reports/goal_progress', data);
+            this.goals = data;
             this.title = this.gettextCatalog.getString(
                 '{{received}} received/{{pledged}} committed of goal: {{goal}}. Click to see outstanding financial partners.', {
-                    pledged: this.totalPledges,
-                    received: this.receivedPledges,
-                    goal: this.monthlyGoal
+                    pledged: this.goals.total_pledges,
+                    received: this.goals.received_pledges,
+                    goal: this.goals.monthly_goal
                 }
             );
-        }
+        });
+        this.api.get('reports/balances', {filter: {account_list_id: this.api.account_list_id}, include: 'designation_accounts'}).then((data) => {
+            this.$log.debug('reports/account_balances', data);
+            this.balances = data;
+            this.balance = _.sum(_.map(data.designation_accounts, 'converted_balance'));
+        });
     }
 }
 
 const Balances = {
     controller: BalancesController,
-    template: require('./balances.html'),
-    bindings: {
-        inHandPercent: '<',
-        monthlyGoal: '<',
-        pledgedPercent: '<',
-        receivedPledges: '<',
-        totalPledges: '<'
-    }
+    template: require('./balances.html')
 };
 
 export default angular.module('mpdx.menu.balances.component', [])
