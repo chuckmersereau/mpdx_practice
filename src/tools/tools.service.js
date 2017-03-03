cecbb2ab5be6e9274a63ecbb923d7a54a2ed099a
Jimmy Beck <beckja.d@gmail.com>
2017-03-03 12:22:27 -0500

    Update tools.service.js (#170)
    updated icons and copy.

--- src/tools/tools.service.js
+++ src/tools/tools.service.js
@@ -5,16 +5,16 @@ class ToolsService {
         gettextCatalog
     ) {
         this.data = [
-            {title: gettextCatalog.getString('Goals & Appeals'), icon: 'fa fa-trophy', link: 'tools.goalsAndAppeals', description: gettextCatalog.getString('Set goals,create asks, and track progress for one time needs.'), enabled: true},
+            {title: gettextCatalog.getString('Goals & Appeals'), icon: 'fa fa-trophy', link: 'tools.goalsAndAppeals', description: gettextCatalog.getString('Set goals, create asks, and track progress for one time needs.'), enabled: true},
             {title: gettextCatalog.getString('Import from Google'), icon: 'fa fa-google', link: 'tools.importFromGoogle', description: gettextCatalog.getString('Import your contact information from your Google account.'), enabled: true},
             {title: gettextCatalog.getString('Import from CSV'), icon: 'fa fa-table', link: 'tools.importFromCSV', description: gettextCatalog.getString('Import contacts you have saved in a .csv spreadsheet.'), enabled: true},
-            {title: gettextCatalog.getString('Import from TntConnect'), icon: 'fa fa-bomb', link: 'tools.importFromTNT', description: gettextCatalog.getString('Import your TntConnect database into MPDX.'), enabled: true},
-            {title: gettextCatalog.getString('Fix Partner Status'), icon: 'fa fa-usd', link: 'tools.fixPartnerStatus', description: gettextCatalog.getString('Let MPDX make sure you have your contacts status up to date.'), enabled: true},
-            {title: gettextCatalog.getString('Fix Phone'), icon: 'fa fa-phone-square', link: 'tools.fixAllData', description: gettextCatalog.getString('Let MPDX make sure you have the correct primary Phone set for a person'), enabled: true},
-            {title: gettextCatalog.getString('Fix Email Address'), icon: 'fa fa-envelope-o', link: 'tools.fixEmailAddress', description: gettextCatalog.getString('Let MPDX make sure you have the correct primary Email set for a person.'), enabled: true},
-            {title: gettextCatalog.getString('Fix Mailing Address'), icon: 'fa fa-map', link: 'tools.fixMailingAddress', description: gettextCatalog.getString('Let MPDX make sure you have the correct primary Address set for a contact.'), enabled: true},
+            {title: gettextCatalog.getString('Import from TntConnect'), icon: 'fa fa-upload', link: 'tools.importFromTNT', description: gettextCatalog.getString('Import your TntConnect database into MPDX.'), enabled: true},
+            {title: gettextCatalog.getString('Fix Commitment Info'), icon: 'fa fa-usd', link: 'tools.fixPartnerStatus', description: gettextCatalog.getString('Help MPDX make sure your contacts commitment info up to date.'), enabled: true},
+            {title: gettextCatalog.getString('Fix Phone'), icon: 'fa fa-phone-square', link: 'tools.fixAllData', description: gettextCatalog.getString('Help MPDX set the correct primary Phone for each person.'), enabled: true},
+            {title: gettextCatalog.getString('Fix Email Address'), icon: 'fa fa-envelope-o', link: 'tools.fixEmailAddress', description: gettextCatalog.getString('Help MPDX set the correct primary Email for each person.'), enabled: true},
+            {title: gettextCatalog.getString('Fix Mailing Address'), icon: 'fa fa-map', link: 'tools.fixMailingAddress', description: gettextCatalog.getString('Help MPDX set the correct primary Address for each contact.'), enabled: true},
             {title: gettextCatalog.getString('Merge Contacts'), icon: 'fa fa-users', link: 'contacts.reconcile_partners', description: gettextCatalog.getString('Review and merge duplicate contacts in your database.'), enabled: true},
-            {title: gettextCatalog.getString('Merge People'), icon: 'fa fa-user-secret', link: 'contacts.reconcile_individuals', description: gettextCatalog.getString('Review and merge duplicate people in your database.'), enabled: true}
+            {title: gettextCatalog.getString('Merge People'), icon: 'fa fa-exchange', link: 'contacts.reconcile_individuals', description: gettextCatalog.getString('Review and merge duplicate people in your database.'), enabled: true}
         ];
     }
 }
