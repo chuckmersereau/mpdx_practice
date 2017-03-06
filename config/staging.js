e79504d5b42c9c0f11886bb05bca476860e04418
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-03-07 10:34:20 +1300

    Add the key (#177)
    * change to the new authenticate endpoint and allow relay
    
    * update oauth credentials
    
    * restore specs
    
    * restore staging api to dev
    
    * use https for stage api on dev
    
    * typo

--- config/staging.js
+++ config/staging.js
@@ -1,6 +1,6 @@
 export default {
     "env": "staging",
-    "apiUrl": "https://api.stage,mpdx.org//api/v2/",
+    "apiUrl": "https://api.stage.mpdx.org/api/v2/",
     "authUrl": "https://thekey.me/cas/",
     "authLoginPath": "login?client_id=1151388713555353789&scope=fullticket&response_type=token&redirect_uri=https://next.stage.mpdx.org/auth"
 };
