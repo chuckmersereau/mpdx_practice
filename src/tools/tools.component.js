30d0a754b6afbdd9db5331949481db215c34f76f
Mike Allison <mike@mikeallisononline.com>
2017-04-05 20:41:34 -0500

    lint fix

--- src/tools/tools.component.js
+++ src/tools/tools.component.js
@@ -7,7 +7,7 @@ class ToolsController {
     ) {
         this.$state = $state;
         this.tools = tools;
-            
+
         help.suggest([
             '58d3d70ddd8c8e7f5974d3ca',
             '5845aa229033600698176a54',
@@ -16,7 +16,7 @@ class ToolsController {
             '58496d4ec6979106d373bb57',
             '58496e389033600698178180',
             '58a47007dd8c8e56bfa7b7a4'
-       ]);
+        ]);
 
         this.setup = $stateParams.setup;
     }
