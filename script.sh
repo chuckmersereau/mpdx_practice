21e7aa586e694cf588ceead73cbbbdfc0cb9c236
Mike Allison <mike@mikeallisononline.com>
2017-04-07 00:29:46 -0500

    move script around for faster PR validation

--- script.sh
+++ script.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -e
+
 echo '-- run tests --'
 npm test
+
 echo '-- run eslint --'
 eslint .
-if [ "$TRAVIS_BRANCH" = "master" ]
-then
-    echo '-- build production --'
-    export NODE_ENV=production
-else
-    echo '-- build staging --'
-    export NODE_ENV=staging
-fi
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
+    if [ "$TRAVIS_BRANCH" = "master" ]
+    then
+        echo '-- build production --'
+        export NODE_ENV=production
+    else
+        echo '-- build staging --'
+        export NODE_ENV=staging
+    fi
+
     echo '-- extract translations from source --'
     gulp extract
 
@@ -23,17 +26,19 @@ then
 
     echo '-- download translation po from onesky --'
     node onesky/download
-else
-    echo 'Skipping translation upload because the current build is a pull request.'
-fi
 
-echo '-- run build --'
-npm run build
+    echo '-- run build --'
+    npm run build
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]
-then
     echo '-- extract language po into angular-gettext json --'
     gulp translations
+
+    echo '<!-- COMMIT:' $TRAVIS_COMMIT '-->' >> public/index.html
+else
+    echo 'Skipping translation and build because the current build is a pull request.'
 fi
 
-echo '<!-- COMMIT:' $TRAVIS_COMMIT '-->' >> public/index.html
+
+
+
+
