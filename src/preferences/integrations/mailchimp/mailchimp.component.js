816627c7a39d9dcfbb95fa1dd02965b3ff105133
John Plastow II <j2trumpet@users.noreply.github.com>
2017-04-13 06:12:35 -0700

    Integration styling (#390)
    * Starting integration styling
    
    * mailchimp updates
    
    * MailChimp styling
    
    * show help method
    
    * Finished MailChimp styling and fixed MailChimp branding
    
    * Google integration styling and cleanup

--- src/preferences/integrations/mailchimp/mailchimp.component.js
+++ src/preferences/integrations/mailchimp/mailchimp.component.js
@@ -6,23 +6,25 @@ class MailchimpIntegrationPreferencesController {
     state;
 
     constructor(
-        $scope, gettextCatalog,
-        mailchimp, alerts
+        gettextCatalog,
+        mailchimp, alerts, help
     ) {
         this.gettextCatalog = gettextCatalog;
+
         this.mailchimp = mailchimp;
+        this.help = help;
         this.alerts = alerts;
+
         this.saving = false;
         this.showSettings = false;
     }
-    save() {
+    save(showSettings = false) {
         this.saving = true;
         this.mailchimp.save().then(() => {
             this.alerts.addAlert(this.gettextCatalog.getString('Preferences saved successfully'), 'success');
             this.saving = false;
-            if (this.mailchimp.data.primary_list_id !== null) {
-                this.hide();
-            }
+            this.showSettings = showSettings;
+            return this.hide(showSettings);
         }).catch((data) => {
             each(value => {
                 this.alerts.addAlert(value, 'danger');
@@ -30,9 +32,9 @@ class MailchimpIntegrationPreferencesController {
             this.saving = false;
         });
     }
-    hide() {
-        this.mailchimp.load(true).then(() => {
-            this.showSettings = false;
+    hide(showSettings = false) {
+        return this.mailchimp.load(true).then(() => {
+            this.showSettings = showSettings;
         });
     }
     sync() {
@@ -48,12 +50,16 @@ class MailchimpIntegrationPreferencesController {
     disconnect() {
         this.saving = true;
         return this.mailchimp.disconnect().then(() => {
+            this.showSettings = false;
             this.saving = false;
             this.alerts.addAlert(this.gettextCatalog.getString('MPDX removed your integration with MailChimp'), 'success');
         }).catch(() => {
             this.alerts.addAlert(this.gettextCatalog.getString('MPDX couldn\'t save your configuration changes for MailChimp'), 'danger');
             this.saving = false;
         });
+    }
+    showHelp() {
+        this.help.showHelp();
     }
 }
 
