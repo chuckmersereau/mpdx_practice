96902c645b883ff07aa4b2635945fa73b4c82f96
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-05-06 11:17:09 +1200

    Google integrations (#497)
    * init google int
    
    * merge staging
    
    * completed Google Integrations

---
+++ src/preferences/integrations/google/integrations/integrations.controller.js
@@ -0,0 +1,51 @@
+class GoogleIntegrationsModalController {
+    googleIntegrations;
+
+    constructor(
+        $log, blockUI, gettextCatalog,
+        googleIntegrations, modal, serverConstants, google,
+        googleIntegration, googleAccount
+    ) {
+        this.$log = $log;
+        this.gettextCatalog = gettextCatalog;
+        this.blockUI = blockUI.instances.get('googleIntegrations');
+
+        this.googleIntegrations = googleIntegrations;
+        this.modal = modal;
+        this.serverConstants = serverConstants;
+        this.google = google;
+
+        this.googleIntegration = googleIntegration;
+        this.googleAccount = googleAccount;
+
+        this.activeTab = 'calendar';
+    }
+    enable(integrationName) {
+        this.blockUI.start();
+        this.googleIntegrations.enable(this.googleAccount, this.googleIntegration, integrationName).then((data) => {
+            this.blockUI.reset();
+            this.googleIntegration = data;
+        });
+    }
+    disable(integrationName) {
+        this.modal.confirm(this.gettextCatalog.getString('Are you sure you want to disable Google {{name}} sync?', { name: integrationName })).then(() => {
+            this.blockUI.start();
+            this.googleIntegrations.disable(this.googleAccount, this.googleIntegration, integrationName).then((data) => {
+                this.blockUI.reset();
+                this.googleIntegration = data;
+            });
+        });
+    }
+    sync(integrationName) {
+        this.blockUI.start();
+        this.googleIntegrations.sync(this.googleAccount, this.googleIntegration, integrationName).then(() => {
+            this.blockUI.reset();
+        });
+    }
+    save() {
+        this.googleIntegrations.save(this.googleAccount, this.googleIntegration);
+    }
+}
+
+export default angular.module('mpdx.preferences.integrations.google.integrations.controller', [])
+    .controller('googleIntegrationsModalController', GoogleIntegrationsModalController).name;
