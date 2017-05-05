bc1618ec4c9bd54baaa9bb7438a892b74eb48e28
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-05-06 03:26:34 +1200

    Google integrations (#492)
    * init google int
    
    * merge staging
    
    * completed Google Integrations

--- src/preferences/integrations/google/index.module.js
+++ src/preferences/integrations/google/index.module.js
@@ -1,7 +1,9 @@
 import component from './google.component';
 import service from './google.service';
+import integrations from './integrations/index.module';
 
 export default angular.module('mpdx.preferences.integrations.google', [
     component,
-    service
+    service,
+    integrations
 ]).name;
