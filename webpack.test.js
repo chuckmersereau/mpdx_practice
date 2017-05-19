750bd3f91f2dba4f4a2a5b8a29974bf6f84c3d66
Mike Allison <dj.mikeallison@gmail.com>
2017-05-19 17:35:20 -0500

    Feature/swap instanbul (#549)
    * swap abandoned isparta for updated istanbul
    update .gitignore
    remove outdated comments on karma.conf
    add cross-env for easy node_env switching
    
    * clean up from nyc attempt

--- webpack.test.js
+++ webpack.test.js
@@ -20,14 +20,6 @@ config = assign(config, {
     module: assign(config.module, {
         loaders: concat(config.module.loaders, [
             {
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: [
-                    /node_modules/,
-                    /\.test\.js$/
-                ],
-                use: 'isparta-loader'
-            }, {
                 test: /\.css$|\.scss$/,
                 use: 'null-loader'
             }
