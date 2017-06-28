8c723cbe3693937a0361a35f5fbb5fb4a8ed3d71
Tataihono Nikora <tataihono.nikora@gmail.com>
2017-06-28 13:34:40 +1200

    MPDX-3797 wildcard search update (#644)
    * search updates for keyboard navigation
    
    * spec updates
    
    * change find index function
    
    * ensure contact list variable is reset on each test

--- src/menu/search/search.component.js
+++ src/menu/search/search.component.js
@@ -1,3 +1,6 @@
+import find from 'lodash/fp/find';
+import findIndex from 'lodash/fp/findIndex';
+
 class ContactsSearchController {
     contacts;
     contactFilter;
@@ -12,26 +15,70 @@ class ContactsSearchController {
         this.contactFilter = contactFilter;
 
         this.searchParams = '';
+        this.contactList = [];
+        this.moreActive = false;
     }
+
     reset() {
-        this.$timeout(() => {
-            this.searchParams = '';
-            this.contactList = null;
-        }, 200);
+        this.searchParams = '';
+        this.contactList = [];
     }
+
     go(contactId) {
         this.reset();
-        this.$state.go('contacts.show', {contactId: contactId});
+        this.$state.go('contacts.show', { contactId: contactId });
     }
+
     gotoList() {
-        this.contactFilter.wildcard_search = angular.copy(this.searchParams);
-        this.$state.go('contacts', {}, {reload: true});
-        this.reset();
+        const activeContact = find('active', this.contactList);
+        if (activeContact) {
+            this.go(activeContact.id);
+        } else {
+            this.contactFilter.wildcard_search = angular.copy(this.searchParams);
+            this.$state.go('contacts', {}, {reload: true});
+            this.reset();
+        }
     }
+
     search() {
-        this.contacts.search(this.searchParams).then((data) => {
+        return this.contacts.search(this.searchParams).then((data) => {
             this.contactList = data;
         });
+    }
+
+    keyup(event) {
+        if ((event.keyCode === 38 || event.keyCode === 40) && this.contactList.length !== 0) {
+            let activeIndex;
+            activeIndex = findIndex({ active: true }, this.contactList);
+            this.moreActive = false;
+            if (activeIndex >= 0) {
+                this.contactList[activeIndex].active = false;
+            }
+            switch (event.keyCode) {
+                case 38:
+                    if (activeIndex >= 0) {
+                        if (this.contactList[activeIndex - 1]) {
+                            this.contactList[activeIndex - 1].active = true;
+                        } else {
+                            this.moreActive = true;
+                        }
+                    } else if (this.contactList.length > 0) {
+                        this.contactList[this.contactList.length - 1].active = true;
+                    }
+                    break;
+                case 40:
+                    if (activeIndex >= 0) {
+                        if (this.contactList[activeIndex + 1]) {
+                            this.contactList[activeIndex + 1].active = true;
+                        } else {
+                            this.moreActive = true;
+                        }
+                    } else if (this.contactList.length > 0) {
+                        this.contactList[0].active = true;
+                    }
+                    break;
+            }
+        }
     }
 }
 const Search = {
