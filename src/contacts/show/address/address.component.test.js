a9f005ac417ade9a257b8464d98e60c0e391e81b
Mike Allison <dj.mikeallison@gmail.com>
2017-07-25 14:59:01 -0500

    MPDX-3975 address_types (#689)
    * initial commit (needs tests)
    
    * unit tests

--- src/contacts/show/address/address.component.test.js
+++ src/contacts/show/address/address.component.test.js
@@ -16,15 +16,38 @@ describe('contacts.show.address.component', () => {
         $ctrl = componentController('contactAddress', {$scope: scope}, {});
     }
     describe('$onChanges', () => {
-        it('should build a google map link', () => {
+        beforeEach(() => {
             $ctrl.address = {
                 street: '123 Street St',
                 city: 'City',
                 state: 'ST',
                 postal_code: '12345'
             };
+        });
+        it('should build a google map link', () => {
             $ctrl.$onChanges();
             expect($ctrl.mapLink).toEqual('https://www.google.com/maps/search/?api=1&query=123 Street St, City, ST, 12345');
+        });
+        it('shouldn\'t be editable with a remote_id', () => {
+            $ctrl.address.remote_id = true;
+            $ctrl.address.source = 'MPDX';
+            $ctrl.$onChanges();
+            expect($ctrl.isEditable).toBeFalsy();
+        });
+        it('should be editable with source MPDX', () => {
+            $ctrl.address.source = 'MPDX';
+            $ctrl.$onChanges();
+            expect($ctrl.isEditable).toBeTruthy();
+        });
+        it('should be editable with source manual', () => {
+            $ctrl.address.source = 'manual';
+            $ctrl.$onChanges();
+            expect($ctrl.isEditable).toBeTruthy();
+        });
+        it('should be editable with source TntImport', () => {
+            $ctrl.address.source = 'TntImport';
+            $ctrl.$onChanges();
+            expect($ctrl.isEditable).toBeTruthy();
         });
     });
 });
