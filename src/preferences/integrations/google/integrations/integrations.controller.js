01aa4bda87766ab52fb4569bcb0d631017fd9df6
Mike Allison <dj.mikeallison@gmail.com>
2017-09-11 16:52:13 -0500

    MPDX-4125_google_blacklist (#788)
    * initial commit
    
    * unit test
    
    * api EntityAttributes unit tests

--- src/preferences/integrations/google/integrations/integrations.controller.js
+++ src/preferences/integrations/google/integrations/integrations.controller.js
@@ -1,21 +1,25 @@
+import concat from 'lodash/fp/concat';
+import pullAt from 'lodash/fp/pullAt';
+
 class GoogleIntegrationsModalController {
     constructor(
         $log, blockUI, gettextCatalog,
-        googleIntegrations, modal, serverConstants, google,
+        alerts, api, googleIntegrations, modal, serverConstants, google,
         googleIntegration, googleAccount
     ) {
         this.$log = $log;
+        this.alerts = alerts;
+        this.api = api;
         this.gettextCatalog = gettextCatalog;
-        this.blockUI = blockUI.instances.get('googleIntegrations');
-
+        this.google = google;
         this.googleIntegrations = googleIntegrations;
         this.modal = modal;
         this.serverConstants = serverConstants;
-        this.google = google;
 
         this.googleIntegration = googleIntegration;
         this.googleAccount = googleAccount;
 
+        this.blockUI = blockUI.instances.get('googleIntegrations');
         this.activeTab = 'calendar';
     }
     enable(integrationName) {
@@ -45,9 +49,35 @@ class GoogleIntegrationsModalController {
         });
     }
     save() {
-        this.googleIntegrations.save(this.googleAccount, this.googleIntegration);
+        return this.api.put({
+            url: `user/google_accounts/${this.googleAccount.id}/google_integrations/${this.googleIntegration.id}`,
+            data: this.googleIntegration,
+            type: 'google_integrations'
+        }).then(() => {
+            this.alerts.addAlert(this.gettextCatalog.getString('Google integration saved.'));
+        }).catch((ex) => {
+            this.alerts.addAlert(this.gettextCatalog.getString('Unable to save Google integration.'), 'danger');
+            throw ex;
+        });
+    }
+    addEmailAddress() {
+        this.googleIntegration.email_blacklist = concat(this.googleIntegration.email_blacklist, null);
+    }
+    removeEmail(index) {
+        this.googleIntegration.email_blacklist = pullAt([index], this.googleIntegration.email_blacklist);
     }
 }
 
-export default angular.module('mpdx.preferences.integrations.google.integrations.controller', [])
-    .controller('googleIntegrationsModalController', GoogleIntegrationsModalController).name;
+import alerts from 'common/alerts/alerts.service';
+import api from 'common/api/api.service';
+import blockUI from 'angular-block-ui';
+import gettextCatalog from 'angular-gettext';
+import google from '../google.service';
+import googleIntegrations from './integrations.service';
+import modal from 'common/modal/modal.service';
+import serverConstants from 'common/serverConstants/serverConstants.service';
+
+export default angular.module('mpdx.preferences.integrations.google.integrations.controller', [
+    blockUI, gettextCatalog,
+    alerts, api, googleIntegrations, modal, serverConstants, google
+]).controller('googleIntegrationsModalController', GoogleIntegrationsModalController).name;
