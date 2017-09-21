81718a1a673014edead96b20c234acd422018d99
Mike Allison <mike@mikeallisononline.com>
2017-09-21 17:56:44 -0500

    api tests

--- src/common/api/api.service.js
+++ src/common/api/api.service.js
@@ -5,58 +5,17 @@ import defaultTo from 'lodash/fp/defaultTo';
 import { EntityAttributes } from './entities';
 import has from 'lodash/fp/has';
 import isArray from 'lodash/fp/isArray';
+import isFunction from 'lodash/fp/isFunction';
 import isNil from 'lodash/fp/isNil';
 import isObject from 'lodash/fp/isObject';
 import japi from 'jsonapi-serializer';
 import joinComma from '../fp/joinComma';
 import map from 'lodash/fp/map';
+import omit from 'lodash/fp/omit';
 import pull from 'lodash/fp/pull';
 import reduceObject from '../fp/reduceObject';
 
-function appendTransform(defaults, transform) {
-    // We can't guarantee that the default transformation is an array
-    defaults = isArray(defaults) ? defaults : [defaults];
-
-    // Append the new transformation to the defaults
-    return concat(defaults, transform);
-}
-
 const jsonApiParams = { keyForAttribute: 'underscore_case' };
-
-function isDataObject(data) {
-    return isObject(data) && data.data;
-}
-
-function deserialize(data, deSerializationOptions) {
-    const options = assign(jsonApiParams, deSerializationOptions);
-    return isDataObject(data)
-        ? new japi.Deserializer(options).deserialize(data).then((deserializedData) => {
-            deserializedData.meta = defaultTo({}, data.meta);
-            return deserializedData;
-        }) : data;
-}
-
-function removeIdIfUndefined(serialized, method) {
-    if (method === 'post' && serialized.data.id === 'undefined') {
-        delete serialized.data.id;
-    }
-    return serialized;
-}
-
-function enablePutOverwrite(serialized, method) {
-    if (method === 'put') {
-        serialized.data.attributes = defaultTo({}, serialized.data.attributes);
-        serialized.data.attributes.overwrite = true;
-    }
-    return serialized;
-}
-
-function serialize(key, params, item, method) {
-    let serialized = new japi.Serializer(key, params).serialize(item);
-    serialized = removeIdIfUndefined(serialized, method);
-    serialized = enablePutOverwrite(serialized, method);
-    return serialized;
-}
 
 class Api {
     constructor(
@@ -101,17 +60,11 @@ class Api {
             data.filter = this.cleanFilters(data.filter);
         }
 
-        if (method === 'get' || method === 'delete') {
-            params = assign(params, data);
-        }
+        params = this.isGetOrDelete(method) ? assign(params, data) : params;
 
         // set jsonapi content type
-        if (!headers['Content-Type']) {
-            headers['Content-Type'] = 'application/vnd.api+json';
-        }
-        if (!headers.Accept) {
-            headers.Accept = 'application/vnd.api+json';
-        }
+        headers['Content-Type'] = defaultTo('application/vnd.api+json', headers['Content-Type']);
+        headers.Accept = defaultTo('application/vnd.api+json', headers.Accept);
 
         // override the browsers language with the one from current user
         headers['Accept-Language'] = this.language;
@@ -124,49 +77,10 @@ class Api {
             headers: headers,
             paramSerializer: '$httpParamSerializerJQLike',
             responseType: responseType,
-            transformRequest: (data) => {
-                let params = angular.copy(jsonApiParams);
-                if (method === 'put' || (method === 'post' && !overrideGetAsPost) || method === 'delete') {
-                    if (!type) {
-                        let arr = url.split('/');
-                        if ((method === 'put' || method === 'delete') && arr.length % 2 === 0) {
-                            type = arr[arr.length - 2];
-                        } else {
-                            type = arr[arr.length - 1];
-                        }
-                    }
-                    if (has(type, this.entityAttributes)) {
-                        params = assign(params, this.entityAttributes[type]);
-                    } else {
-                        this.$log.error(`undefined attributes for model: ${type} in api.service`);
-                    }
-                }
-                if (doSerialization) {
-                    if (isArray(data)) {
-                        return angular.toJson({
-                            data: map((item) => serialize(type, params, item, method), data)
-                        });
-                    } else {
-                        return angular.toJson(serialize(type, params, data, method));
-                    }
-                } else {
-                    return angular.toJson(data);
-                }
-            },
-            transformResponse: appendTransform(this.$http.defaults.transformResponse, (data) => {
-                if (beforeDeserializationTransform) {
-                    data = beforeDeserializationTransform(data);
-                }
-                if (doDeSerialization) {
-                    if (isArray(data)) {
-                        return map((item) => deserialize(item, deSerializationOptions), data);
-                    } else {
-                        return deserialize(data, deSerializationOptions);
-                    }
-                } else {
-                    return data;
-                }
-            }),
+            transformRequest: (data) => this.transformRequest(data, url, method, type, overrideGetAsPost,
+                doSerialization),
+            transformResponse: this.transformResponse(beforeDeserializationTransform, doDeSerialization,
+                deSerializationOptions),
             cacheService: false,
             timeout: 50000
         };
@@ -179,6 +93,15 @@ class Api {
         });
 
         return promise.promise;
+    }
+    isGetOrDelete(method) {
+        return method === 'get' || method === 'delete';
+    }
+    isPutOrDelete(method) {
+        return method === 'put' || method === 'delete';
+    }
+    isPutPostOrDelete(method, overrideGetAsPost) {
+        return this.isPutOrDelete(method) || (method === 'post' && !overrideGetAsPost);
     }
     get(...params) {
         params = assign(this.handleParamsAsOther(params), { method: 'get' });
@@ -205,6 +128,50 @@ class Api {
         }
         return params;
     }
+    transformRequest(data, url, method, type, overrideGetAsPost, doSerialization) {
+        let params = angular.copy(jsonApiParams);
+        if (this.isPutPostOrDelete(method, overrideGetAsPost)) {
+            if (!type) {
+                let arr = url.split('/');
+                if (this.isPutOrDelete(method) && arr.length % 2 === 0) {
+                    type = arr[arr.length - 2];
+                } else {
+                    type = arr[arr.length - 1];
+                }
+            }
+            if (has(type, this.entityAttributes)) {
+                params = assign(params, this.entityAttributes[type]);
+            } else {
+                this.$log.error(`undefined attributes for model: ${type} in api.service`);
+            }
+        }
+        return doSerialization
+            ? this.serializeData(data, type, params, method)
+            : angular.toJson(data);
+    }
+    serializeData(data, type, params, method) {
+        return isArray(data)
+            ? angular.toJson({
+                data: map((item) => this.serialize(type, params, item, method), data)
+            })
+            : angular.toJson(this.serialize(type, params, data, method));
+    }
+    transformResponse(beforeDeserializationTransform, doDeSerialization, deSerializationOptions) {
+        return this.appendTransform(this.$http.defaults.transformResponse, (data) =>
+            this.afterTransform(data, beforeDeserializationTransform, doDeSerialization, deSerializationOptions));
+    }
+    afterTransform(data, beforeDeserializationTransform, doDeSerialization, deSerializationOptions) {
+        data = this.doBeforeSerialization(beforeDeserializationTransform, data);
+        return doDeSerialization ? this.deserializeData(data, deSerializationOptions) : data;
+    }
+    deserializeData(data, deSerializationOptions) {
+        return isArray(data)
+            ? map((item) => this.deserialize(item, deSerializationOptions), data)
+            : this.deserialize(data, deSerializationOptions);
+    }
+    doBeforeSerialization(beforeDeserializationTransform, data) {
+        return isFunction(beforeDeserializationTransform) ? beforeDeserializationTransform(data) : data;
+    }
     encodeURLarray(array) {
         return map(encodeURIComponent, array);
     }
@@ -220,6 +187,41 @@ class Api {
             }
             return result;
         }, {}, filter);
+    }
+    appendTransform(defaults, transform) {
+        // We can't guarantee that the default transformation is an array
+        defaults = isArray(defaults) ? defaults : [defaults];
+
+        // Append the new transformation to the defaults
+        return concat(defaults, transform);
+    }
+    deserialize(data, deSerializationOptions) {
+        const options = assign(jsonApiParams, deSerializationOptions);
+        return this.isDataObject(data)
+            ? new japi.Deserializer(options).deserialize(data).then((deserializedData) => {
+                deserializedData.meta = defaultTo({}, data.meta);
+                return deserializedData;
+            })
+            : data;
+    }
+    isDataObject(data) {
+        return isObject(data) && data.data;
+    }
+    removeIdIfUndefined(serialized, method) {
+        return method === 'post' && serialized.data.id === 'undefined' ? omit(['data.id'], serialized) : serialized;
+    }
+    serialize(key, params, item, method) {
+        let serialized = new japi.Serializer(key, params).serialize(item);
+        serialized = this.removeIdIfUndefined(serialized, method);
+        serialized = this.enablePutOverwrite(serialized, method);
+        return serialized;
+    }
+    enablePutOverwrite(serialized, method) {
+        if (method === 'put') {
+            serialized.data.attributes = defaultTo({}, serialized.data.attributes);
+            serialized.data.attributes.overwrite = true;
+        }
+        return serialized;
     }
 }
 
--- src/common/api/api.test.js
+++ src/common/api/api.test.js
@@ -1,4 +1,5 @@
 import service from './api.service';
+import assign from 'lodash/fp/assign';
 
 const jsonApiResponse = {
     data: {
@@ -6,9 +7,23 @@ const jsonApiResponse = {
     }
 };
 
+const jsonApiArrayResponse = {
+    data: [{
+        id: 234
+    }, {
+        id: 345
+    }]
+};
+
 const transformedResponse = {
     id: 234
 };
+
+const transformedArrayResponse = [{
+    id: 234
+}, {
+    id: 345
+}];
 
 const jsonApiErrorResponse = {
     errors: [{
@@ -87,11 +102,137 @@ describe('common.api.service', () => {
             });
             $httpBackend.flush();
         });
+        it('should send a simple delete request', () => {
+            $httpBackend.expectDELETE('/api/v1/contacts/1').respond(200, jsonApiResponse);
+
+            api.delete('contacts/1').then((data) => {
+                expect(data).toEqual(transformedResponse);
+            }).catch(() => {
+                fail('should have returned Success');
+            });
+            $httpBackend.flush();
+        });
     });
 
     describe('encodeURLarray', () => {
         it('should encode an array of values', () => {
             expect(api.encodeURLarray(['handles spaces', '?&'])).toEqual([ 'handles%20spaces', '%3F%26' ]);
+        });
+    });
+
+    describe('enablePutOverwrite', () => {
+        it('should add overwrite', () => {
+            expect(api.enablePutOverwrite({ data: { attributes: { id: 1 } } }, 'put')).toEqual({ data: { attributes: { id: 1, overwrite: true } } });
+        });
+        it('shouldn\'t add overwrite', () => {
+            expect(api.enablePutOverwrite({ data: { attributes: { id: 1 } } }, 'post')).toEqual({ data: { attributes: { id: 1 } } });
+        });
+    });
+
+    describe('appendTransform', () => {
+        it('should create an array', () => {
+            expect(api.appendTransform('a', 'b')).toEqual(['a', 'b']);
+        });
+        it('should handle an array', () => {
+            expect(api.appendTransform(['a'], 'b')).toEqual(['a', 'b']);
+        });
+    });
+
+    describe('cleanFilters', () => {
+        it('should remove empty strings', () => {
+            expect(api.cleanFilters({
+                a: ''
+            })).toEqual({});
+        });
+        it('should pull empty strings from array', () => {
+            expect(api.cleanFilters({
+                a: ['', 'b', 'c']
+            })).toEqual({ a: 'b,c' });
+        });
+    });
+
+    describe('removeIdIfUndefined', () => {
+        it('should remove undefined objects', () => {
+            expect(api.removeIdIfUndefined({ data: { id: 'undefined' } }, 'post')).toEqual({ data: {} });
+        });
+        it('shouldn\'t remove defined objects', () => {
+            expect(api.removeIdIfUndefined({ data: { id: '1' } }, 'post')).toEqual({ data: { id: '1' } });
+        });
+    });
+
+    describe('deserialize', () => {
+        it('should deserialize valid data', (done) => {
+            spyOn(api, 'isDataObject').and.callFake(() => true);
+            api.deserialize(jsonApiResponse, {}).then((data) => {
+                expect(api.isDataObject).toHaveBeenCalledWith(jsonApiResponse);
+                expect(data).toEqual(assign(transformedResponse, { meta: {} }));
+                done();
+            });
+        });
+        it('should deserialize valid data', () => {
+            spyOn(api, 'isDataObject').and.callFake(() => false);
+            expect(api.deserialize({ a: 'b' }, {})).toEqual({ a: 'b' });
+            expect(api.isDataObject).toHaveBeenCalledWith({ a: 'b' });
+        });
+    });
+
+    describe('isDataObject', () => {
+        it('should return true if object and contains data attribute', () => {
+            expect(api.isDataObject({ data: {} })).toBeTruthy();
+        });
+        it('should return false if object and doesn\'t contain data attribute', () => {
+            expect(api.isDataObject({})).toBeFalsy();
+        });
+        it('should return false if not an object', () => {
+            expect(api.isDataObject('a')).toBeFalsy();
+        });
+    });
+    describe('doBeforeSerialization', () => {
+        it('should run the function', () => {
+            const doStuff = () => 'a';
+            expect(api.doBeforeSerialization(doStuff, {})).toEqual('a');
+        });
+        it('should return data', () => {
+            const doStuff = 'a';
+            expect(api.doBeforeSerialization(doStuff, {})).toEqual({});
+        });
+    });
+    describe('deserializeData', () => {
+        beforeEach(() => {
+            spyOn(api, 'deserialize').and.callFake(() => 'a');
+        });
+        it('should deserialize an object', () => {
+            expect(api.deserializeData({ id: 1 }, 'b')).toEqual('a');
+        });
+        it('should deserialize an array', () => {
+            expect(api.deserializeData([{ id: 1 }, { id: 2 }], 'b')).toEqual(['a', 'a']);
+            expect(api.deserialize).toHaveBeenCalledWith({ id: 1 }, 'b');
+            expect(api.deserialize).toHaveBeenCalledWith({ id: 2 }, 'b');
+        });
+    });
+    describe('transformResponse', () => {
+        beforeEach(() => {
+            spyOn(api, 'appendTransform').and.callFake(() => Promise.resolve());
+        });
+        it('should call appendTransform', (done) => {
+            api.transformResponse(null, true, {}).then(() => {
+                expect(api.appendTransform).toHaveBeenCalledWith(api.$http.defaults.transformResponse,
+                    jasmine.any(Function));
+                done();
+            });
+        });
+    });
+    describe('afterTransform', () => {
+        beforeEach(() => {
+            spyOn(api, 'doBeforeSerialization').and.callFake(() => 'a');
+            spyOn(api, 'deserializeData').and.callFake(() => 'b');
+        });
+        it('should deserialize', () => {
+            expect(api.afterTransform({ id: 1 }, null, false)).toEqual('a');
+            expect(api.doBeforeSerialization).toHaveBeenCalledWith(null, { id: 1 });
+        });
+        it('should not deserialize', () => {
+            expect(api.afterTransform({ id: 1 }, null, true)).toEqual('b');
         });
     });
 });
