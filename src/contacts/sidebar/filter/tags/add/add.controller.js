199a04009e1bfa30999f1abea967b280a0a053f6
Mike Allison <dj.mikeallison@gmail.com>
2017-10-05 12:42:43 -0500

    MPDX-4205 bulk_add_contact_tag (#843)
    * initial commit
    
    * unit test
    
    * unit test

--- src/contacts/sidebar/filter/tags/add/add.controller.js
+++ src/contacts/sidebar/filter/tags/add/add.controller.js
@@ -1,4 +1,3 @@
-import isArray from 'lodash/fp/isArray';
 import map from 'lodash/fp/map';
 
 class AddTagController {
@@ -17,26 +16,29 @@ class AddTagController {
         this.tags = [];
     }
     save(tag) {
-        let tagToAdd = tag || map((tag) => { return { name: tag }; }, this.tags);
+        const tagToAdd = tag ? [tag] : this.tags;
         if (!tagToAdd) {
             return;
         }
-        if (!isArray(tagToAdd)) {
-            tagToAdd = [tagToAdd];
-        }
+
         return this.api.post({
             url: 'contacts/tags/bulk',
-            data: tagToAdd,
-            params: {
+            data: {
+                data: map((tag) => ({
+                    data: {
+                        type: 'tags',
+                        attributes: { name: tag }
+                    }
+                }), tagToAdd),
                 filter: {
                     account_list_id: this.api.account_list_id,
                     contact_ids: this.selectedContacts.join()
                 }
             },
-            type: 'tags'
+            doSerialization: false
         }).then(() => {
-            const tag = { tags: map((t) => ({ name: t }), tagToAdd), contactIds: this.selectedContacts };
-            this.$rootScope.$emit('contactTagsAdded', tagToAdd);
+            const tag = { tags: tagToAdd, contactIds: this.selectedContacts };
+            this.$rootScope.$emit('contactTagsAdded', tag);
             this.contactsTags.addTag(tag);
             this.$scope.$hide();
         });
