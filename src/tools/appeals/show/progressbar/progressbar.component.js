b6c246b3a257320e0cb38cb5e16aec29c2f046bd
Mike Allison <dj.mikeallison@gmail.com>
2017-10-30 12:34:23 -0500

    new progressbar display calculations (#908)

--- src/tools/appeals/show/progressbar/progressbar.component.js
+++ src/tools/appeals/show/progressbar/progressbar.component.js
@@ -6,6 +6,20 @@ class ProgressbarController {
     }
     $onChanges() {
         this.maxValue = this.appeal.amount > 0 ? this.appeal.amount : 1;
+        let available = 100;
+        this.givenWidth = this.getWidth(this.appeal.pledges_amount_processed);
+
+        available -= this.givenWidth;
+        available = available < 0 ? 0 : available;
+
+        const receivedWidth = this.getWidth(this.appeal.pledges_amount_received_not_processed);
+        this.receivedWidth = receivedWidth > available ? available : receivedWidth;
+
+        available -= this.receivedWidth;
+        available = available < 0 ? 0 : available;
+
+        const committedWidth = this.getWidth(this.appeal.pledges_amount_not_received_not_processed);
+        this.committedWidth = committedWidth > available ? available : committedWidth;
     }
     getWidth(value) {
         return floor((value / this.maxValue) * 100);
--- src/tools/appeals/show/progressbar/progressbar.component.test.js
+++ src/tools/appeals/show/progressbar/progressbar.component.test.js
@@ -12,10 +12,15 @@ describe('tools.appeals.show.progressbar.component', () => {
         });
     });
     describe('$onChanges', () => {
-        it('should set max value to amount', () => {
+        beforeEach(() => {
             $ctrl.appeal = {
-                amount: 5000
+                amount: 5000,
+                pledges_amount_processed: 2000,
+                pledges_amount_received_not_processed: 1000,
+                pledges_amount_not_received_not_processed: 5000
             };
+        });
+        it('should set max value to amount', () => {
             $ctrl.$onChanges();
             expect($ctrl.maxValue).toEqual(5000);
         });
@@ -25,6 +30,36 @@ describe('tools.appeals.show.progressbar.component', () => {
             };
             $ctrl.$onChanges();
             expect($ctrl.maxValue).toEqual(1);
+        });
+        it('should calculate givenWidth', () => {
+            $ctrl.$onChanges();
+            expect($ctrl.givenWidth).toEqual(40);
+        });
+        it('should calculate receivedWidth', () => {
+            $ctrl.$onChanges();
+            expect($ctrl.receivedWidth).toEqual(20);
+        });
+        it('should calculate committedWidth', () => {
+            $ctrl.$onChanges();
+            expect($ctrl.committedWidth).toEqual(40);
+        });
+        it('should handle overage on received as well', () => {
+            $ctrl.appeal.pledges_amount_processed = 4500;
+            $ctrl.$onChanges();
+            expect($ctrl.receivedWidth).toEqual(10);
+            expect($ctrl.committedWidth).toEqual(0);
+        });
+        it('should handle negative availability', () => {
+            $ctrl.appeal.pledges_amount_processed = 45000;
+            $ctrl.$onChanges();
+            expect($ctrl.receivedWidth).toEqual(0);
+            expect($ctrl.committedWidth).toEqual(0);
+        });
+        it('should handle negative availability', () => {
+            $ctrl.appeal.pledges_amount_received_not_processed = 45000;
+            $ctrl.$onChanges();
+            expect($ctrl.receivedWidth).toEqual(60);
+            expect($ctrl.committedWidth).toEqual(0);
         });
     });
     describe('getWidth', () => {
