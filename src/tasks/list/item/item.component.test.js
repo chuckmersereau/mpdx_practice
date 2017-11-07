019506d81f89f475d0608b3345213f889bc00702
Mike Allison <mike@mikeallisononline.com>
2017-11-07 15:27:13 -0600

    fix unit tests

--- src/tasks/list/item/item.component.test.js
+++ src/tasks/list/item/item.component.test.js
@@ -101,8 +101,9 @@ describe('tasks.list.item.component', () => {
         it('should query the api for a tasks comments & contacts info', () => {
             $ctrl.load();
             expect(api.get).toHaveBeenCalledWith('tasks/1', {
-                include: 'comments,comments.person,contacts,contacts.addresses,contacts.people,contacts.people.facebook_accounts,contacts.people.phone_numbers,contacts.people.email_addresses',
+                include: 'activity_contacts,comments,comments.person,contacts,contacts.addresses,contacts.people,contacts.people.facebook_accounts,contacts.people.phone_numbers,contacts.people.email_addresses',
                 fields: {
+                    activity_contacts: 'contact',
                     contacts: 'addresses,name,status,square_avatar,send_newsletter,pledge_currency_symbol,pledge_frequency,pledge_received,uncompleted_tasks_count,tag_list,pledge_amount,people',
                     addresses: 'city,historic,primary_mailing_address,postal_code,state,source,street',
                     email_addresses: 'email,historic,primary',
