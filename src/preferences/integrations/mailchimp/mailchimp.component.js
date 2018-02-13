5b389c3d08b498e732315d80a8827497f05e4fc8
Mike Allison <mike@mikeallisononline.com>
2018-02-13 10:06:24 -0600

    add prod flag for mailchimp

--- src/preferences/integrations/mailchimp/mailchimp.component.js
+++ src/preferences/integrations/mailchimp/mailchimp.component.js
@@ -17,6 +17,7 @@ class MailchimpIntegrationPreferencesController {
 
         this.saving = false;
         this.showSettings = false;
+        this.isProduction = config.env === 'production';
 
         $rootScope.$on('accountListUpdated', () => {
             this.mailchimp.load();
