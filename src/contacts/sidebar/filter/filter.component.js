f05a41078758820ca1434656bfc699764560b828
Mike Allison <dj.mikeallison@gmail.com>
2018-03-21 14:58:48 -0500

    commit (#1084)

--- src/contacts/sidebar/filter/filter.component.js
+++ src/contacts/sidebar/filter/filter.component.js
@@ -1,9 +1,12 @@
-import { assign } from 'lodash/fp';
+import { assign, find, get } from 'lodash/fp';
+import { split } from 'common/fp/strings';
+import reduceObject from 'common/fp/reduceObject';
+import { stringToNameObjectArray } from 'common/fp/tags';
 
 class FilterController {
     constructor(
         $stateParams, gettextCatalog,
-        contactFilter, contactsTags, contacts, filters, modal
+        contactFilter, contactsTags, contacts, filters, modal, users
     ) {
         this.contacts = contacts;
         this.contactFilter = contactFilter;
@@ -11,6 +14,7 @@ class FilterController {
         this.filters = filters;
         this.modal = modal;
         this.gettextCatalog = gettextCatalog;
+        this.users = users;
 
         this.dateRangeLocale = {
             applyLabel: this.gettextCatalog.getString('Filter'),
@@ -29,6 +33,37 @@ class FilterController {
             this.contactFilter.reset();
         }
     }
+    useSavedFilter(name) {
+        const option = this.users.getCurrentOptionValue(`saved_contacts_filter_${name}`);
+        const value = JSON.parse(option);
+        const params = reduceObject((result, value, key) => {
+            const filter = find({ name: key }, this.contactFilter.data);
+            const isMultiselect = get('type', filter) === 'multiselect';
+            result[key] = isMultiselect ? split(',', value) : value;
+            return result;
+        }, {}, value.params);
+        this.contactFilter.params = assign(this.contactFilter.default_params, params);
+        this.contactFilter.wildcard_search = value.wildcard_search;
+        this.contactsTags.any_tags = value.any_tags;
+        this.contactsTags.rejectedTags = stringToNameObjectArray(value.exclude_tags);
+        this.contactsTags.selectedTags = stringToNameObjectArray(value.tags);
+        this.contactFilter.change();
+        this.contactFilter.selectedSave = name;
+    }
+    openSaveModal() {
+        this.modal.open({
+            controller: 'saveFilterModal',
+            template: require('../../../common/filters/save/save.html'),
+            locals: {
+                anyTags: this.contactsTags.any_tags,
+                filterType: 'contacts',
+                params: this.contactFilter.params,
+                rejectedTags: this.contactsTags.rejectedTags,
+                selectedTags: this.contactsTags.selectedTags,
+                wildcardSearch: this.contactFilter.wildcard_search
+            }
+        });
+    }
 }
 
 const Filter = {
@@ -36,5 +71,11 @@ const Filter = {
     template: require('./filter.html')
 };
 
-export default angular.module('mpdx.contacts.filter.component', [])
-    .component('contactsFilter', Filter).name;
+import contacts from 'contacts/contacts.service';
+import contactFilter from './filter.service';
+import modal from 'common/modal/modal.service';
+import users from 'common/users/users.service';
+
+export default angular.module('mpdx.contacts.filter.component', [
+    contactFilter, contacts, modal, users
+]).component('contactsFilter', Filter).name;
