720791ec408ca58b4e8cd0290a1cf361ff1b6172
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-04-12 15:36:08 -0400

    tab fixes

--- src/contacts/show/show.component.js
+++ src/contacts/show/show.component.js
@@ -63,7 +63,7 @@ class ContactController {
         }
 
         this.sortableOptions = {
-            containment: '#contact-tabs',
+            containment: '#contact-tabs .horizontal-tab-sortable',
             // restrict move across columns. move only within column.
             accept: (sourceItemHandleScope, destSortableScope) =>
                 sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id,
