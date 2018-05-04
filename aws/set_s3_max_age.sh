b8c84205ef9e3ba768631397ffadf53148830e16
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-05-04 11:26:07 -0400

    add public acl

--- aws/set_s3_max_age.sh
+++ aws/set_s3_max_age.sh
@@ -4,17 +4,17 @@ export PATH=$PATH:$HOME/.local/bin
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
   echo 'set max-age header on index.html in cru-mpdx-web-prod'
-  aws s3 cp s3://cru-mpdx-web-prod/index.html s3://cru-mpdx-web-prod/index.html --metadata-directive REPLACE --cache-control max-age=0,public
+  aws s3 cp s3://cru-mpdx-web-stage/index.html s3://cru-mpdx-web-stage/index.html --metadata-directive REPLACE --cache-control max-age=0,public --acl public-read
 fi
 
 if [ "$TRAVIS_BRANCH" = "next" ]
 then
   echo 'set max-age header on index.html in cru-mpdx-web-next'
-  aws s3 cp s3://cru-mpdx-web-next/index.html s3://cru-mpdx-web-next/index.html --metadata-directive REPLACE --cache-control max-age=0,public
+  aws s3 cp s3://cru-mpdx-web-next/index.html s3://cru-mpdx-web-next/index.html --metadata-directive REPLACE --cache-control max-age=0,public --acl public-read
 fi
 
 if [ "$TRAVIS_BRANCH" = "staging" ]
 then
   echo 'set max-age header on index.html in cru-mpdx-web-stage'
-  aws s3 cp s3://cru-mpdx-web-stage/index.html s3://cru-mpdx-web-stage/index.html --metadata-directive REPLACE --cache-control max-age=0,public
+  aws s3 cp s3://cru-mpdx-web-stage/index.html s3://cru-mpdx-web-stage/index.html --metadata-directive REPLACE --cache-control max-age=0,public --acl public-read
 fi
