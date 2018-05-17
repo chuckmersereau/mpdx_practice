89bb38e0978cdd07b5bf477e34f64558e6e17c46
Mike Allison <mike@mikeallisononline.com>
2018-05-17 15:43:10 -0500

    remove service import

--- src/common/filters/filters.module.ts
+++ src/common/filters/filters.module.ts
@@ -2,8 +2,7 @@ import header from './header/header.component';
 import headerFilter from './header/filter/filter.component';
 import save from './save/save.controller';
 import saved from './saved/saved.component';
-import service from './filters.service';
 
 export default angular.module('mpdx.common.filters', [
-    header, headerFilter, save, saved, service
+    header, headerFilter, save, saved
 ]).name;
