9ac8e8d20b72c48b22e9ebc6dc1a4b1511e19466
Mike Allison <mike@mikeallisononline.com>
2018-05-18 12:26:58 -0500

    re-enable filters (hopefully)

--- src/common/filters/filters.module.ts
+++ src/common/filters/filters.module.ts
@@ -1,11 +1,11 @@
-import header from './header/header.component';
-// import headerFilter from './header/filter/filter.component';
-// import save from './save/save.controller';
-// import saved from './saved/saved.component';
+// import header from './header/header.component';
+import headerFilter from './header/filter/filter.component';
+import save from './save/save.controller';
+import saved from './saved/saved.component';
 
 export default angular.module('mpdx.common.filters', [
-    header
-    // headerFilter,
-    // save,
-    // saved
+    // header
+    headerFilter,
+    save,
+    saved
 ]).name;
