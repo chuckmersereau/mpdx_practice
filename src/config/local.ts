97576424376dfc59ebd2f478447961f47a377ec2
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-07-18 14:08:45 +1200

    add local configuration (#1263)
    store this command in package saving api devs time.

--- src/config/local.ts
+++ src/config/local.ts
@@ -6,5 +6,5 @@ export default {
     'authLoginPath': 'login?client_id=4027334344069527005&scope=fullticket&response_type=token&redirect_uri=http://localhost:8080/auth',
     'authSignupPath': 'service/selfservice?target=signup&client_id=4027334344069527005&scope=fullticket&response_type=token&redirect_uri=http://localhost:8080/auth',
     'authLogout': 'logout?service=http://localhost:8080/login',
-    'oAuthUrl': 'http://localhost:3000/auth/user/'
+    'oAuthUrl': 'http://auth.lvh.me:3000/auth/user/'
 };
