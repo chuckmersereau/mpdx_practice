15f456df38e58a7927bceaefd0361e5cd6352b49
Mike Allison <dj.mikeallison@gmail.com>
2018-08-02 14:31:42 -0500

    MPDX-5358 adblock (#1284)

--- src/common/help/help.service.ts
+++ src/common/help/help.service.ts
@@ -27,51 +27,58 @@ export function HelpMock($provide) {
 export class HelpService {
     constructor(
         $log: ng.ILogService,
+        private $window: ng.IWindowService,
         private gettextCatalog: ng.gettext.gettextCatalog
     ) {
         if (config.env === 'test') {
             (window as any).HS = HSMock;
             return;
         }
-        HS.beacon.config({
-            color: '#05699B',
-            icon: 'question',
-            topArticles: true,
-            translation: {
-                searchLabel: this.gettextCatalog.getString('What can we help you with?'),
-                searchErrorLabel: this.gettextCatalog.getString('Your search timed out. Please double-check your internet connection and try again.'),
-                noResultsLabel: this.gettextCatalog.getString('No results found for'),
-                contactLabel: this.gettextCatalog.getString('Send a Message'),
-                attachFileLabel: this.gettextCatalog.getString('Attach a file'),
-                attachFileError: this.gettextCatalog.getString('The maximum file size is 10mb'),
-                fileExtensionError: this.gettextCatalog.getString('The file format you uploaded is not allowed.'),
-                nameLabel: this.gettextCatalog.getString('Your Name'),
-                nameError: this.gettextCatalog.getString('Please enter your name'),
-                emailLabel: this.gettextCatalog.getString('Email address'),
-                emailError: this.gettextCatalog.getString('Please enter a valid email address'),
-                topicLabel: this.gettextCatalog.getString('Select a topic'),
-                topicError: this.gettextCatalog.getString('Please select a topic from the list'),
-                subjectLabel: this.gettextCatalog.getString('Subject'),
-                subjectError: this.gettextCatalog.getString('Please enter a subject'),
-                messageLabel: this.gettextCatalog.getString('How can we help you?'),
-                messageError: this.gettextCatalog.getString('Please enter a message'),
-                sendLabel: this.gettextCatalog.getString('Send'),
-                contactSuccessLabel: this.gettextCatalog.getString('Message sent!'),
-                contactSuccessDescription: this.gettextCatalog.getString('Thanks for reaching out! Someone from our team will get back to you soon.')
-            },
-            zIndex: 10003,
-            poweredBy: false,
-            showSubject: true,
-            autoInit: true
-        });
         try {
+            HS.beacon.config({
+                color: '#05699B',
+                icon: 'question',
+                topArticles: true,
+                translation: {
+                    searchLabel: this.gettextCatalog.getString('What can we help you with?'),
+                    searchErrorLabel: this.gettextCatalog.getString('Your search timed out. Please double-check your internet connection and try again.'),
+                    noResultsLabel: this.gettextCatalog.getString('No results found for'),
+                    contactLabel: this.gettextCatalog.getString('Send a Message'),
+                    attachFileLabel: this.gettextCatalog.getString('Attach a file'),
+                    attachFileError: this.gettextCatalog.getString('The maximum file size is 10mb'),
+                    fileExtensionError: this.gettextCatalog.getString('The file format you uploaded is not allowed.'),
+                    nameLabel: this.gettextCatalog.getString('Your Name'),
+                    nameError: this.gettextCatalog.getString('Please enter your name'),
+                    emailLabel: this.gettextCatalog.getString('Email address'),
+                    emailError: this.gettextCatalog.getString('Please enter a valid email address'),
+                    topicLabel: this.gettextCatalog.getString('Select a topic'),
+                    topicError: this.gettextCatalog.getString('Please select a topic from the list'),
+                    subjectLabel: this.gettextCatalog.getString('Subject'),
+                    subjectError: this.gettextCatalog.getString('Please enter a subject'),
+                    messageLabel: this.gettextCatalog.getString('How can we help you?'),
+                    messageError: this.gettextCatalog.getString('Please enter a message'),
+                    sendLabel: this.gettextCatalog.getString('Send'),
+                    contactSuccessLabel: this.gettextCatalog.getString('Message sent!'),
+                    contactSuccessDescription: this.gettextCatalog.getString('Thanks for reaching out! Someone from our team will get back to you soon.')
+                },
+                zIndex: 10003,
+                poweredBy: false,
+                showSubject: true,
+                autoInit: true
+            });
             HS.beacon.init();
         } catch (err) {
+            // only debug log as we don't want rollbar cluttered by people using adblockers
             $log.debug('Help Service', err);
         }
     }
     showHelp(): void {
-        HS.beacon.open();
+        try {
+            HS.beacon.open();
+        } catch (err) {
+            // fallback to opening help in a new tab
+            this.$window.open('https://mpdx.helpscoutdocs.com', '_blank');
+        }
     }
     showArticle(articleId: string): void {
         HS.beacon.show(articleId);
